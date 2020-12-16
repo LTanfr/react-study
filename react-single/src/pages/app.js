08c139c02c4bc3f489fda10d9bdca1cf26645670
LTanfr <576480782@qq.com>
2020-12-16 13:14:09 +0800

    react router2

--- react-single/src/pages/app.js
+++ react-single/src/pages/app.js
@@ -1,5 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './app.scss';
+//import {Button} from 'antd';
+//import 'antd/dist/antd.css'
 
 export default function App(){
-    return <div>This is App</div>
+    return <div class="container">
+        <h1>Tan</h1>
+        <Link to="/login">点击跳转登录页面</Link>
+        <br/>
+        <Link to="/home">点击跳转主页面</Link>
+        <br/>
+    </div>
 }
+// export default class App extends React.Component{
+//     handleJump = ()=>{
+//         this.props.history.push('./login');
+//     }
+//     render(){
+//         return <div class="container">
+//         <h1>Tan</h1>
+//         <Link to="/login">点击跳转登录页面</Link>
+//         <br/>
+//         <Link to="/home">点击跳转主页面</Link>
+//         <br/>
+//         <Button onClick={this.handleJump}>跳转登录</Button>
+//     </div>        
+//     }
+// }
--- react-single/src/router.js
+++ react-single/src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 import App from './pages/app';
 import Login from './pages/login';
 import Home from './pages/home'
@@ -7,8 +7,10 @@ import Home from './pages/home'
 
 export default function IRouter(){
     return <Router>
-        <Route path="/" component={App}></Route>
-        <Route path="/login" component={Login}></Route>
-        <Route path="/home" component={Home}></Route>
+        <Switch>
+            <Route exact path="/" component={App}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/home" component={Home}></Route>
+        </Switch>
     </Router>
 }
